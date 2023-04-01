4193ad32403c0f00d3b2c4e5c4918b4870701b32
Leo Yeh <leoyeh@Leos-M-MacBook-Air.local>
2023-04-01 15:34:43 -0400

    complete problems

--- js/video.js
+++ js/video.js
@@ -21,18 +21,18 @@ document.querySelector("#pause").addEventListener("click", function() {
 })
 
 document.querySelector("#slower").addEventListener("click", function() {
-	video.playbackRate *= 0.95;
-	console.log("New speed: " + video.playbackRate);
+	video.playbackRate *= 0.90;
+	console.log("New speed is " + video.playbackRate);
 })
 
 document.querySelector("#faster").addEventListener("click", function() {
-	video.playbackRate /= 0.95;
-	console.log("New speed: " + video.playbackRate);
+	video.playbackRate /= 0.90;
+	console.log("New speed is " + video.playbackRate);
 })
 
 document.querySelector("#skip").addEventListener("click", function() {
-	video.currentTime += 15;
-	if (currentTime >= video.duration){
+	video.currentTime += 10;
+	if (video.currentTime >= video.duration){
 		video.currentTime = 0;
 	}
 	console.log("Current location: " + video.currentTime);
@@ -40,12 +40,28 @@ document.querySelector("#skip").addEventListener("click", function() {
 
 document.querySelector("#mute").addEventListener("click", function() {
 	if (video.muted) {
-		video.volume = 1;
-	} else {
-		video.volume = 0;
+		video.muted = false;
+		this.textContent = "Mute";
+		console.log('Unmute')
+	}else{
+		video.muted = true;
+		this.textContent = "Unmute";
+		console.log('Mute')
 	}
 })
 
+document.querySelector("#slider").addEventListener("change", function() {
+    video.volume = this.value / 100;
+	console.log(this.value / 100);
+	document.querySelector('#volume').innerHTML = (video.volume * 100) + "%";
+})
 
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool");
+	console.log("Vintage");
+})
 
-
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove("oldSchool");
+	console.log("Original");
+})
